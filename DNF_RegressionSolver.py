3c33b2abbcedc549e3b9d84bbf5bca0a3f91f7be
tokob9999 <96751911+tomkob9999@users.noreply.github.com>
2024-01-12 07:59:35 +0900

    Add files via upload

--- DNF_RegressionSolver.py
+++ DNF_RegressionSolver.py
@@ -1,6 +1,6 @@
 # Name: DNF_Regression_solver
 # Author: tomio kobayashi
-# Version: 2.2.2
+# Version: 2.2.3
 # Date: 2024/01/12
 
 import itertools
@@ -18,6 +18,10 @@ class DNF_Regression_solver:
 # false unmatch exists and there is no corresponding other rule
     def __init__(self):
         self.expression = ""
+        self.expression_common = ""
+        self.expression_true = ""
+        self.expression_false = ""
+        self.expression_union = ""
         
     def cnf_to_dnf(cnf):
         dnf_clauses = []
@@ -62,7 +66,7 @@ class DNF_Regression_solver:
             exec(__tokens___[__jjjj___] + " = " + str(__ccc___[__jjjj___]))
         return eval(self.expression)
             
-    def solve(self, inp, check_negative=True):
+    def solve(self, inp, check_negative=True, used_expression=""):
         numvars = len(inp[0])
         
         if check_negative:
@@ -77,6 +81,18 @@ class DNF_Regression_solver:
 #         inp_list = np.array([np.array([inp[i][j] for j in range(len(inp[i]))]) for i in range(1, len(inp), 1)])
         inp_list = [[inp[i][j] for j in range(len(inp[i]))] for i in range(1, len(inp), 1)]
         res = list(range(len(inp_list)))
+        
+        if self.expression == "" or used_expression == "common":
+            self.expression = self.expression_common
+        elif used_expression == "true":
+            self.expression = self.expression_true
+        elif used_expression == "false":
+            self.expression = self.expression_false
+        elif used_expression == "union":
+            self.expression = self.expression_union
+            
+        print("Solver Expression:")
+        print(self.expression)
         for i in range(len(inp_list)):
             res[i] = self.myeval(inp_list[i], tokens)
 
@@ -87,6 +103,9 @@ class DNF_Regression_solver:
     #     print("df", df)
         df[name + '_rank'] = pd.cut(df[name], bins=num_segments, labels=False)
         df[name + '_label'] = pd.cut(df[name], bins=num_segments, labels=[f'{name} {i+1}' for i in range(num_segments)])
+        min_max_per_group = df.groupby(name + '_rank')[name].agg(['max'])
+        print("")
+        print(min_max_per_group)
         return df
 
     def discretize_data(data_list, by_four=1):
@@ -94,17 +113,12 @@ class DNF_Regression_solver:
     #     lst = [['apple', 'red', 11], ['grape', 'green', 22], ['orange', 'orange', 33], ['mango', 'yellow', 44]] 
     #     data = pd.DataFrame(values, columns =headers, dtype = float) 
     
-#         print("data_list", data_list)
         result_header = data_list[0][-1]
         result_values = [d[-1] for d in data_list[1:]]
         
         headers = data_list[0][:-2]
         values = [d[:-2] for d in data_list[1:]]
-        
-#         print("result_header", result_header)
-#         print("result_values", result_values)
-#         print("headers", headers)
-#         print("values", values)
+
     
         data = pd.DataFrame(values, columns=headers) 
 
@@ -112,10 +126,7 @@ class DNF_Regression_solver:
         cols = [c for c in data.columns]
         for c in cols:
             countNonBool = len(data[c]) - (data[c] == 0).sum() - (data[c] == 1).sum()
-#             countNonBool = len(data[c]) - (data[c] == 0).sum() - (data[c] == 1).sum() - (data[c] == "0").sum() - (data[c] == "1").sum()
-#             print("countNonBool", countNonBool)
             if countNonBool > 0 and pd.api.types.is_numeric_dtype(data[c]):
-#             if countNonBool > 0:
                 result_df = DNF_Regression_solver.generate_segment_ranks(data, by_four*4, c)
                 one_hot_df = pd.get_dummies(result_df[c + '_rank'], prefix=c)
                 one_hot_df = one_hot_df.astype(int)
@@ -130,26 +141,17 @@ class DNF_Regression_solver:
         new_cols = []
         for c in cols:
             countNonBool = len(data[c]) - (data[c] == 0).sum() - (data[c] == 1).sum()
-#             countNonBool = len(data[c]) - (data[c] == 0).sum() - (data[c] == 1).sum() - (data[c] == "0").sum() - (data[c] == "1").sum()
             if countNonBool == 0 and pd.api.types.is_numeric_dtype(data[c]):
-#             if countNonBool == 0:
                 new_cols.append(c)
 
-#         print(new_cols)
-#         print("new_cols", new_cols)
         data = data.filter(items=new_cols)
 
         data_list = [data.columns.tolist()] + data.values.tolist()
 
         
-#         print("result_header", result_header)
-#         print("result_values", result_values)
-        
         data_list[0].append(result_header)
         for i in range(len(result_values)):
             data_list[i+1].append(result_values[i])
-    
-#         print("data_list", data_list)
         
         return data_list
         
@@ -173,11 +175,12 @@ class DNF_Regression_solver:
             inp = data_list
 
         print("Discretizing...")
-#         print("inp", inp)
         inp = [[DNF_Regression_solver.try_convert_to_numeric(inp[i][j]) for j in range(len(inp[i]))] for i in range(len(inp))]
-#         print("inp", inp)
         inp = DNF_Regression_solver.discretize_data(inp)
-#         print("inp", inp)
+        print("")
+        print("Columns:")
+        print(inp[0])
+        print("")
         
         numvars = len(inp[1])-1
 
@@ -186,7 +189,6 @@ class DNF_Regression_solver:
                 inp[0].insert(i+numvars, "n_" + inp[0][i])
             for j in range(1, len(inp), 1):
                 for i in range(numvars):
-#                     inp[j].insert(i+numvars,"0" if inp[j][i] == "1" else "1")
                     inp[j].insert(i+numvars, 0 if inp[j][i] == 1 else 1)
             numvars *= 2
 
@@ -202,7 +204,6 @@ class DNF_Regression_solver:
         for i in range(1, len(inp), 1):
             s = ""
             for j in range(len(inp[i]) - 1):
-#                 s += inp[i][j]
                 s += str(inp[i][j])
             truefalse = inp[i][len(inp[i]) - 1]
             dic[int(s, 2)] = truefalse
@@ -212,10 +213,6 @@ class DNF_Regression_solver:
                 false_list.append(s)
 
         inp_oppo = [copy.deepcopy(inp[0])] + [[0 if m == 1 else 1 for m in inp[i]] for i in range(1, len(inp), 1)]
-        
-#         print("inp", inp[0:5])
-#         print("inp_oppo", inp_oppo[0:5])
-        
         for i in range(1, len(inp_oppo), 1):
             s = ""
             for j in range(len(inp_oppo[i]) - 1):
@@ -223,19 +220,8 @@ class DNF_Regression_solver:
                 s += str(inp_oppo[i][j])
             truefalse = inp_oppo[i][len(inp_oppo[i]) - 1]
             dic_opp[int(s, 2)] = truefalse
-#             if truefalse == '1':
-#                 true_list.append(s)
-#             else:
-#                 false_list.append(s)
-
-#         print("len(dic)", len(dic))
-#         print("len(dic_opp)", len(dic_opp))
-#         print("dic", dic)
-#         print("dic_opp", dic_opp)
                 
         print("Deriving true expressions...")
-#         print("true_list", true_list)
-#         print("false_list", false_list)
         dnf_perf = list()
         raw_perf = list()
         raw_perf2 = list()
@@ -261,9 +247,7 @@ class DNF_Regression_solver:
                 r = [v for k,v in dic.items() if k & b == b]
                 if len(r) == 0:
                     continue
-#                 if len([f for f in r if f == "0"]) > 0:
                 cnt_all = len([f for f in r])
-#                 cnt_unmatch = len([f for f in r if f == "0"])
                 cnt_unmatch = len([f for f in r if f == 0])
                 if cnt_unmatch/cnt_all > error_tolerance:
                     continue
@@ -272,8 +256,6 @@ class DNF_Regression_solver:
                 raw_perf2.append(b)
         
         for dn in raw_perf:
-#             dnf_perf.append([inp[0][ii] for ii in dn])
-#             print([inp[0][ii] for ii in dn])
             dnf_perf.append(sorted(list(set([inp[0][ii] for ii in dn]))))
 
         print("size of true dnf " + str(len(dnf_perf)))
@@ -306,9 +288,7 @@ class DNF_Regression_solver:
                         r = [v for k,v in dic.items() if k & b == b]
                         if len(r) == 0:
                             continue
-    #                     if len([f for f in r if f == "1"]) > 0:
                         cnt_all = len([f for f in r])
-    #                     cnt_unmatch = len([f for f in r if f == "1"])
                         cnt_unmatch = len([f for f in r if f == 1])
                         if cnt_unmatch/cnt_all > error_tolerance:
                             continue
@@ -338,28 +318,21 @@ class DNF_Regression_solver:
                         r = [v for k,v in dic_opp.items() if k & b == b]
                         if len(r) == 0:
                             continue
-#                         print("r", r)
-        #                 if len([f for f in r if f == "0"]) > 0:
                         cnt_all = len([f for f in r])
-        #                 cnt_unmatch = len([f for f in r if f == "0"])
                         cnt_unmatch = len([f for f in r if f == 0])
                         if cnt_unmatch/cnt_all > error_tolerance:
                             continue
 
                         raw_perf_n.append([ii for ii in p_list[i]])
                         raw_perf2_n.append(b)  
-                
-        
-#         print("len(raw_perf_n)", len(raw_perf_n))
         
         for dn in raw_perf_n:
-            dnf_perf_n.append([inp[0][ii] for ii in dn])
+            dnf_perf_n.append(sorted(list(set([inp[0][ii] for ii in dn]))))
 
         
         print("size of false dnf in negative form " + str(len(dnf_perf_n)))
         if len(dnf_perf_n) > 10000:
             print("cutting to only 10000 of false dnf in negative form randomly as they are too many")
-#             dnf_perf_n = dnf_perf_n[0:300]
             dnf_perf_n = random.sample(dnf_perf_n, 10000)
         
         set_dnf_true = set(["(" + s + ")" for s in [" & ".join(a) for a in dnf_perf]])
@@ -373,36 +346,24 @@ class DNF_Regression_solver:
             else:
                 cnf = "(" + ") & (".join([" | ".join(a) for a in [[a for a in aa] for aa in dnf_perf_n]]) + ")"
                 cnf_list = [(a for a in aa) for aa in dnf_perf_n]
-#             print("Simplifying CNF for false expressions..")
 #             cnf = str(boolalg.to_cnf(cnf, simplify=True, force=True))
-#             print("CNF", cnf)
 #             set_dnf_false = set([word.strip() for word in str(boolalg.to_dnf(cnf, simplify=True, force=True)).split("|")])
             print("Converting from CNF to DNF for false expressions..")
             my_dnf = DNF_Regression_solver.cnf_to_dnf(cnf_list)
-#             print("my_dnf", my_dnf)
             print("Simplifying CNF for false expressions..")
-#             cnf = simplify(cnf)
-#             print(cnf)
-#             set_dnf_false = set([word.strip() for word in str(boolalg.to_dnf(cnf, simplify=True, force=True)).split("|")])
-#             set_dnf_false = set(my_dnf)
-#             set_dnf_false = set([" & ".join(a) for a in [[a for a in aa] for aa in my_dnf]])
             set_dnf_false = set(["(" + " & ".join(a) + ")" for a in [[a for a in aa] for aa in my_dnf]])
-#             set_dnf_false = set(["(" + s + ")" for s in [" & ".join(a) for a in my_dnf]])
-        
-#             print("CNF", "(" + ") & (".join([" | ".join(a) for a in [[a[2:] if a[0:2] == "n_" else "n_" + a for a in aa] for aa in dnf_perf_n]]) + ")")
-#             set_dnf_false = set([word.strip() for word in str(boolalg.to_dnf("(" + ") & (".join([" | ".join(a) for a in [[a[2:] if a[0:2] == "n_" else "n_" + a for a in aa] for aa in dnf_perf_n]]) + ")")).split("|")])
-
-#             if len(set_dnf_false) == 1 and list(set_dnf_false)[0] != "(":
-#                 set_dnf_false = set(["(" + list(set_dnf_false)[0] + ")"])
             dnf_common = set_dnf_true & set_dnf_false
-            print("DNF conversion completed")
 
         else:
             dnf_common = set_dnf_true
-            
-        self.expression = " | ".join(dnf_common) if check_false else " | ".join(set_dnf_true)
-#         print("set_dnf_true", set_dnf_true)
-#         print("set_dnf_false", set_dnf_false)
+        
+        self.expression = " | ".join(dnf_common)
+        
+        self.expression_common = " | ".join(dnf_common)
+        self.expression_true = " | ".join(set_dnf_true)
+        self.expression_false = " | ".join(set_dnf_false)
+        self.expression_union = " | ".join(set_dnf_true | set_dnf_false)
+        
             
         if check_false:
             print("")
@@ -430,10 +391,12 @@ class DNF_Regression_solver:
         not_picked = [inp[0][ii] for ii in range(len(inp[0])-1) if inp[0][ii] not in perm_vars]
 
         print("")
-#         print("Unsolved variables - " + str(len(not_picked)) + "/" + str(len(perm_vars)))
         print("Unsolved variables - " + str(len(not_picked)) + "/" + str(len(inp[0])-1))
         print("--------------------------------")
         print(not_picked)
+
+
+
 
 
 file_path = '/kaggle/input/tomio2/dnf_regression.txt'
